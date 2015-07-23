3ca88a5f14d182739eeb0e01561e50610fd9394f
anthonywittemann <awittema@usc.edu>
2015-07-23 11:46:20 -0700

    made the code more readable

--- cachematrix.R
+++ cachematrix.R
@@ -1,45 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## makeCacheMatrix creates a list containing a function to
-# 1. set the value of the matrix
-# 2. get the value of the matrix
-# 3. set the value of inverse of the matrix
-# 4. get the value of inverse of the matrix
+## makeCacheMatrix creates a list containing functions to
+# 1) set the value of the matrix
+# 2) get the value of the matrix
+# 3) set the value of inverse of the matrix
+# 4) get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-    
     inv <- NULL
-    set <- function(y) {
+    setMatrix <- function(y) {
         x <<- y
         inv <<- NULL
     }
-    get <- function() x
+    getMatrix <- function() x
     setInverse <- function(inverse) inv <<- inverse
     getInverse <- function() inv
-    list(set=set, get=get, setinverse=setInverse, getinverse=getInverse)
-    
-    # example returning the mean
-    #m <- NULL
-    #set <- function(y) {
-    #    x <<- y
-    #    m <<- NULL
-    #}
-    #get <- function() x
-    #setmean <- function(mean) m <<- mean
-    #getmean <- function() m
-    #list(set = set, get = get,
-    #     setmean = setmean,
-    #     getmean = getmean)
+    list(set=setMatrix, get=getMatrix, setinverse=setInverse, getinverse=getInverse)
 }
 
 
-# The following function returns the inverse of the matrix. It first checks if
-# the inverse has already been computed. If so, it gets the result and skips the
-# computation. If not, it computes the inverse, sets the value in the cache via
-# setinverse function.
+# cacheSolve returns the inverse of the matrix. First, it checks if
+# the inverse has already been computed in the cache. If so, it returns the result. 
+# If not, it computes the inverse, sets the value in the cache with the setInverse function.
 
-# This function assumes that the matrix is always invertible.
+# NOTE: cacheSolve assumes that the matrix is always invertible.
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
@@ -53,15 +35,4 @@ cacheSolve <- function(x, ...) {
     inv <- solve(data)
     x$setInverse(inv)
     inv
-    
-    # example with returning the mean instead of the inverse
-    #m <- x$getmean()
-    #if(!is.null(m)) {
-    #    message("getting cached data")
-    #    return(m)
-    #}
-    #data <- x$get()
-    #m <- mean(data, ...)
-    #x$setmean(m)
-    #m
 }
